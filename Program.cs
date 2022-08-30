3aa0ea05729753fc0cd49f8087803eae3eb6e060
JaylanEsquivel <jaylan1997@hotmail.com>
2022-08-30 14:14:58 -0300

    Base Inicial

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿string opcaoUsuario = ObterOpcaoUser();
+
+while (opcaoUsuario.ToUpper() != "X")
+{
+    switch (opcaoUsuario)
+    {
+        case "1":
+            //TODO Adicionar Alunos
+            break;
+        case "2":
+            //TODO Listar Alunos
+            break;
+        case "3":
+            //TODO IMprime a Media Geral
+            break;
+        default:
+            throw new ArgumentOutOfRangeException();
+    }
+
+   opcaoUsuario = ObterOpcaoUser();
+
+}
+
+static string ObterOpcaoUser()
+{
+    Console.WriteLine("Informe a Opção desejada :");
+    Console.WriteLine();
+    Console.WriteLine("1- Novo Aluno");
+    Console.WriteLine("2- Listar Alunos");
+    Console.WriteLine("3- Calcular Média Geral");
+    Console.WriteLine("X- Sair");
+    Console.WriteLine();
+
+    string opcaoUsuario = Console.ReadLine();
+    return opcaoUsuario;
+}
